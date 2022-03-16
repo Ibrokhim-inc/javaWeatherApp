b44976d0c8fe1d40ddc670e4d2147cf9f979f17a
Ibrohim <ibrohim.bakhtiyorov23@gmail.com>
2022-03-16 15:19:05 +0500

    my first project is uploded

---
+++ app/src/main/java/com/example/obhavo/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.obhavo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText ucer_field;
+    private Button main_btn;
+    private TextView info_pogoda;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ucer_field = findViewById(R.id.ucer_field);
+        main_btn = findViewById(R.id.main_btn);
+        info_pogoda = findViewById(R.id.info_pogoda);
+
+        main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ucer_field.getText().toString().trim().equals(""))
+                    Toast.makeText(MainActivity.this, R.string.text_if, Toast.LENGTH_LONG).show();
+                else {
+                    String city = ucer_field.getText().toString();
+                    String key = "0d9491fa024fece3f2010500baaa07da";
+                    String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=metric&lang=ru";
+                    new GetURLData().execute(url);
+
+                }
+            }
+        });
+    }
+
+    private class GetURLData extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            info_pogoda.setText("Ожидайте...");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null)
+                    buffer.append(line).append("\n");
+
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null)
+                    connection.disconnect();
+                try {
+                    if (reader != null)
+                        reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPostExecute(String result) {
+            if (result!=null){
+                try {
+                    super.onPostExecute(result);
+                    JSONObject jsonObject = new JSONObject(result);
+                    info_pogoda.setText("Темпиратура: " + jsonObject.getJSONObject("main").getDouble("temp")+"\n"
+                            + jsonObject.getJSONArray("weather").getJSONObject(0).getString("description"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                info_pogoda.setText(R.string.text_er_name);
+                Toast.makeText(MainActivity.this, R.string.text_er_rep, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
